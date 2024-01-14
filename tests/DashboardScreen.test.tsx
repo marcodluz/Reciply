abbfdb36e555d0aa8ab0cd9194d083442c85d415
Marco Luz <marcotdl@hotmail.com>
2024-01-14 21:39:27 +0000

    Added an example for Dashboard testing

--- tests/DashboardScreen.test.tsx
+++ tests/DashboardScreen.test.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react-native";
+import renderer from "react-test-renderer";
 import DashboardScreen from "../app/screens/Protected/DashboardScreen";
 
 describe("DashboardScreen", () => {
-  it("renders the header text and icons correctly", () => {
-    const { getByText } = render(<DashboardScreen />);
-    expect(getByText("Hi Marco")).toBeTruthy();
-    expect(getByText("What would you like to view?")).toBeTruthy();
-
-    // Check for menu items by text
-    expect(getByText("Ingredients")).toBeTruthy();
-    expect(getByText("Recipes")).toBeTruthy();
-    expect(getByText("Shopping List")).toBeTruthy();
-    expect(getByText("Settings")).toBeTruthy();
+  it("renders the screen", () => {
+    const screen = renderer.create(<DashboardScreen />).toJSON();
+    expect(screen.children.length).not.toBe(0);
   });
 });
